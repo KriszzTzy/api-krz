b3f1f113635241ade9616201e423521fac57506b
KriszzTzy <kriszztzyofficial@gmail.com>
2025-06-12 03:48:10 +0700

    Update index.js

--- my-api/index.js
+++ my-api/index.js
@@ -1,1 +1,10 @@
-woi
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from Kriszz API!');
+});
+
+app.listen(3000, () => {
+  console.log('API aktif di port 3000');
+});
